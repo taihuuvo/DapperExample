9de51e03b21f16f4d4ffc8acae1c400df8ef7537
Tai Vo <tai.vo@xero.com>
2021-01-22 17:38:02 +1100

    Added Client class

---
+++ DapperConsoleApp/DapperConsoleApp/Client.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DapperConsoleApp
+{
+    public class Client
+    {
+        public string Name { get; set; }
+        public string Information { get; set; }
+    }
+}
